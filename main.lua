d5ea5b0f984c3e0b0df8617c163f3d56bad27b95
Ryzh <gooddmitriy@mail.ru>
2025-04-20 20:42:35 +0300

    in

---
+++ main.lua
@@ -0,0 +1,26 @@
+local MODE = {
+   CLIENT = 0,
+   SERVER = 1
+}
+
+local mode = nil
+
+for _, arg in ipairs(arg) do
+   if arg == "--server" then
+      mode = MODE.SERVER
+   elseif arg == "--client" then
+      mode = MODE.CLIENT
+   end
+end
+
+if not mode then
+   mode = MODE.CLIENT
+end
+
+if mode == MODE.SERVER then
+   print("Run in server mode")
+   require 'server'
+elseif mode == MODE.CLIENT then
+   print("Run in client mode")
+   require 'client'
+end
